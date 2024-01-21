70d43724fc65e2e889831203a1afd4654806f917
Farfadh <farfadh@gmail.com>
2024-01-21 12:05:16 +0100

    Add time to debug/log console output

--- src/routers/DebugLog.ts
+++ src/routers/DebugLog.ts
@@ -3,9 +3,19 @@ import { Router } from './utils';
 
 const logSchema = z.object({ message: z.string().min(1), data: z.unknown().optional() });
 
+function getTime(): string {
+  const date = new Date();
+  return `${pad(date.getHours())}:${pad(date.getMinutes())}:${pad(date.getSeconds())}`;
+}
+
+function pad(value: number): string {
+  return value < 10 ? `0${value}` : String(value);
+}
+
 export const DebugLogRouter = Router((me) => {
   me.post('/debug/log', (req, res) => {
-    console.log('\nLog from:', req.headers['x-forwarded-for'] || req.socket.remoteAddress);
+    const from = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log('\nLog at', getTime(), 'from:', from);
     console.log('Agent:', req.headers['user-agent']);
     try {
       const body = logSchema.parse(req.body);
