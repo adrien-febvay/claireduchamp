e2202a878edbdea4266ca3e85d040af9b8106b13
Adrien Febvay <adrien.febvay@gmail.com>
2024-08-14 13:50:33 +0200

    Check configuration upon production build

--- webpack/index.js
+++ webpack/index.js
@@ -89,10 +89,14 @@ if (args.includes('help')) {
     }
   }
   env = { ...envs[''], ...env };
+  process.env.NODE_ENV = env.NODE_ENV;
   console.log('Environment:');
   for (const [key, value] of Object.entries(env)) {
     console.log(`  ${key}=${value}`);
   }
   run.build('conf', env);
+  if (env.NODE_ENV === 'production') {
+    require('../.dist-tmp/conf');
+  }
   run.build('be', env, env.BE_MODE === 'build' ? 'Build' : 'Serve');
 }
