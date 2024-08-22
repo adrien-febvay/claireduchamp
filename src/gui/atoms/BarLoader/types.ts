abc1dfa94ee95a685c987320b1c8c4190d9b5e7d
Adrien Febvay <adrien.febvay@gmail.com>
2024-08-22 16:11:11 +0200

    Remove unused <BarLoader> atom

--- src/gui/atoms/BarLoader/types.ts
+++
@@ -1,9 +0,0 @@
-import type { BarLoader as Component } from '.';
-
-declare module '.' {
-  namespace BarLoader {
-    interface Props extends React.DivAttributes {}
-  }
-
-  type Props = Component.Props;
-}
