d5441105a08960439ada327bfae875ce65797423
Adrien Febvay <adrien.febvay@gmail.com>
2024-11-26 16:43:39 +0100

    Add [alt] attribute on <img> of <Project.ThumbnailGrid>

--- src/gui/screens/Project/ThumbnailGrid/index.tsx
+++ src/gui/screens/Project/ThumbnailGrid/index.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { arrayGen } from '@/gui/utils/array/gen';
 import { pad as padNumber } from '@/gui/utils/number/pad';
 import { Link } from '@/gui/atoms/Link';
@@ -10,23 +11,29 @@ const BASEPATH = '/img/projects/claire-duchamp';
 
 export const Screen_Project_ThumbnailGrid: React.FC<Props> = (props) => {
   const { photoIndex, project, ...divAttrs } = props;
-  const { id, pictures } = project;
+
+  const [translate] = useTranslation(Project.namespace);
+
+  const { caption, id, pictures, title } = project;
   const copyrights: Dict<string> | null = pictures.copyrights;
   const path = `${BASEPATH}-${pictures.basename}`;
   const pad = padNumber.accordingTo(pictures.count);
+  const thumbnail = translate('thumbnail');
   const imgs = arrayGen(pictures.count, (index) => {
     const copyright = copyrights[index + 1] ?? pictures.copyrights[0];
     const suffix = copyright ? `--${copyright}` : '';
-    return `${path}--${pad(index + 1)}${suffix}.jpg`;
+    const src = `${path}--${pad(index + 1)}${suffix}.jpg`;
+    const alt = `${title} - ${caption} - ${thumbnail} ${index + 1}`;
+    return { src, alt };
   });
 
   return (
     <div classNames={styles.root} {...divAttrs}>
-      {imgs.map((src, index) => {
+      {imgs.map((imgAttrs, index) => {
         const photo = String(index + 1);
         return (
           <Link key={index} to={Project[id]} params={{ photo }} preventScrollReset>
-            <img src={src} className={styles.thumbnail} />
+            <img className={styles.thumbnail} {...imgAttrs} />
           </Link>
         );
       })}
