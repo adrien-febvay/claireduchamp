44cb34977e66966de3696cceb8b5619750222a2f
Adrien Febvay <adrien.febvay@gmail.com>
2024-11-26 16:13:55 +0100

    Adjust title tags of <Project>

--- src/gui/screens/Project/index.tsx
+++ src/gui/screens/Project/index.tsx
@@ -81,7 +81,7 @@ const ProjectScreen: React.FC = () => {
       <div ref={me.ref.content} className={styles.content}>
         <div className={styles.header} ref={me.ref.header}>
           <div className={styles.properties}>
-            <h6>{project.caption}</h6>
+            <h2>{project.caption}</h2>
             <h1>{project.title}</h1>
             {project.info && <p>{project.info}</p>}
           </div>
