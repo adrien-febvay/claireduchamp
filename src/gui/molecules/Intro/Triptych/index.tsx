7ec2b3b7038c0bf5b998669ee00c3e279c22f608
Adrien Febvay <adrien.febvay@gmail.com>
2024-12-06 10:34:05 +0100

    Fix <Intro.Tryptich> showing only one <Image>

--- src/gui/molecules/Intro/Triptych/index.tsx
+++ src/gui/molecules/Intro/Triptych/index.tsx
@@ -1,12 +1,15 @@
 import { Image } from '../Image';
 
+import styles from './styles.scss';
+
 export function Molecule_Intro_Triptych(props: Props) {
   const { pictureKey, ...imgProps } = props;
   return (
-    <div>
-      {[...Array<undefined>(3)].map((_na, subindex) => (
-        <Image key={subindex} landscape pictureKey={`${pictureKey}-${subindex + 1}`} {...imgProps} />
-      ))}
+    <div className={styles.root}>
+      {[...Array<undefined>(3)].map((_na, subindex) => {
+        const pictureSubkey = `${pictureKey}-${subindex + 1}`;
+        return <Image key={subindex} className={styles.image} landscape pictureKey={pictureSubkey} {...imgProps} />;
+      })}
     </div>
   );
 }
